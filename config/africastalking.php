6db710d940b1c5805c27275fe3ffd3695c31ee3c
stephenjude <stephenjudesuccess@gmail.com>
2021-04-19 09:26:16 +0100

    fix code style

--- config/africastalking.php
+++ config/africastalking.php
@@ -1,20 +1,19 @@
 <?php
 
 return [
-
-    /**
+    /*
      * Username from Africastalking
-     * 
+     *
      */
     'username' => env('AFRICASTALKING_USERNAME'),
 
-    /**
+    /*
      * Api key from Africastalking
      *
      */
     'key' => env('AFRICASTALKING_KEY'),
 
-    /**
+    /*
      * Shortcode for sending SMS from Africastalking
      *
      */
