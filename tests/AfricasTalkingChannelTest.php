1d140b6c88ba1f6e892e1b7d1e217a3865a63c8f
Ahmed Nour Jamal ElDin <ite.anjd@outlook.com>
2022-06-28 14:42:00 +0300

    support AfricasTalkingChannel::class

--- tests/AfricasTalkingChannelTest.php
+++ tests/AfricasTalkingChannelTest.php
@@ -3,6 +3,8 @@
 namespace NotificationChannels\AfricasTalking\Test;
 
 use AfricasTalking\SDK\AfricasTalking as AfricasTalkingSDK;
+use AfricasTalking\SDK\SMS;
+use Illuminate\Notifications\AnonymousNotifiable;
 use Illuminate\Notifications\Notification;
 use Mockery;
 use NotificationChannels\AfricasTalking\AfricasTalkingChannel;
@@ -14,13 +16,14 @@ class AfricasTalkingChannelTest extends TestCase
     /** @var Mockery\Mock */
     protected $africasTalking;
 
-    /** @var \NotificationChannels\Twitter\AfricasTalkingChannel */
+    /** @var \NotificationChannels\AfricasTalking\AfricasTalkingChannel */
     protected $channel;
 
     public function setUp(): void
     {
         parent::setUp();
         $this->africasTalking = Mockery::mock(AfricasTalkingSDK::class);
+        $this->sms = Mockery::mock(SMS::class);
         $this->channel = new AfricasTalkingChannel($this->africasTalking);
     }
 
@@ -32,17 +35,77 @@ class AfricasTalkingChannelTest extends TestCase
     }
 
     /** @test */
-    public function it_can_send_sms_notification()
+    public function it_can_send_sms_notification_to_notifiable_with_method()
     {
-        $this->africasTalking->shouldReceive('send')
+        $this->africasTalking->expects('sms')
+            ->once()
+            ->andReturn($this->sms);
+
+        $this->sms->expects('send')
             ->once()
             ->andReturn(200);
 
-        $this->channel->send(new TestNotifiable, new TestNotification);
+        $this->channel->send(new NotifiableWithMethod, new TestNotification);
+    }
+
+    /** @test */
+    public function it_can_send_sms_notification_to_anonymous_notifiable_using_class_name()
+    {
+        $this->africasTalking->expects('sms')
+            ->once()
+            ->andReturn($this->sms);
+
+        $this->sms->expects('send')
+            ->once()
+            ->andReturn(200);
+
+        $this->channel->send((new AnonymousNotifiable())->route(AfricasTalkingChannel::class, "+1111111111"), new TestNotification);
+    }
+
+    /** @test */
+    public function it_can_send_sms_notification_to_anonymous_notifiable_using_string_name()
+    {
+        $this->africasTalking->expects('sms')
+            ->once()
+            ->andReturn($this->sms);
+
+        $this->sms->expects('send')
+            ->once()
+            ->andReturn(200);
+
+        $this->channel->send((new AnonymousNotifiable())->route('africasTalking', "+1111111111"), new TestNotification);
+    }
+
+    /** @test */
+    public function it_can_send_sms_notification_to_notifiable_with_attribute()
+    {
+        $this->africasTalking->expects('sms')
+            ->once()
+            ->andReturn($this->sms);
+
+        $this->sms->expects('send')
+            ->once()
+            ->andReturn(200);
+
+        $this->channel->send(new NotifiableWithAttribute(), new TestNotification);
+    }
+
+    /** @test */
+    public function it_can_send_sms_notification_to_message_get_to()
+    {
+        $this->africasTalking->expects('sms')
+            ->once()
+            ->andReturn($this->sms);
+
+        $this->sms->expects('send')
+            ->once()
+            ->andReturn(200);
+
+        $this->channel->send(new AnonymousNotifiable(), new TestNotificationWithGetTo);
     }
 }
 
-class TestNotifiable
+class NotifiableWithMethod
 {
     use \Illuminate\Notifications\Notifiable;
 
@@ -65,5 +128,38 @@ class TestNotification extends Notification
     public function toAfricasTalking($notifiable)
     {
         return new AfricasTalkingMessage();
+    }
+}
+
+class TestNotificationWithGetTo extends Notification
+{
+    /**
+     * @param $notifiable
+     * @return AfricasTalkingMessage
+     * @throws CouldNotSendNotification
+     */
+    public function toAfricasTalking($notifiable)
+    {
+        return (new AfricasTalkingMessage())
+            ->to('+22222222222');
+    }
+}
+
+
+class Notifiable
+{
+    public $phone_number = null;
+
+    public function routeNotificationFor()
+    {
+    }
+}
+
+class NotifiableWithAttribute
+{
+    public $phone_number = '+22222222222';
+
+    public function routeNotificationFor()
+    {
     }
 }
